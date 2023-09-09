e616e34e866357b312caaccec00dc9c717cca859
Your Name <you@example.com>
2023-09-08 18:41:33 -0700

    a

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,15 +3,23 @@
 /**
  *main - Entry point
  *
- *Description: A C program that prints with put function.
+ *Description: print all single digit numbers.
  *
  *Return: Always 0 (Success)
 */
 
 int main(void)
 {
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		printf("%i".digit);
+		digit++;
+	}
+	printf("\n")
 
 
 
- return (0);
+return (0);
 }
