857e5c30f25b091f319922a42182a8877fb093e6
Your Name <you@example.com>
2023-09-12 19:08:53 -0700

    a

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: .
+ *
+ * Return: Always 0 (Success)
+*/
+	void print_alphabet_x10(void);
+{
+	int line. ch;
+
+	for (line = 0; line <= 'z'; ch++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+
+		_putchar(ch);
+	_putchar('\n');
+	}
+}
+
+
+
+
+
