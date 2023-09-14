1c84bd26ea6a90ba176a7cc6916d3733a693cf34
Your Name <you@example.com>
2023-09-13 17:39:57 -0700

    a

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - function
+ * @c: checks input
+ * description: return 1 if 'c' is lowecase
+ * return: return 0 or 1
+*/
+
+int _islower(int c);
+
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	return (0);
+
+
+}
