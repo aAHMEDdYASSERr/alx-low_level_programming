bd06bfea1e5b1513074e2ede8cc5f154b5202dfd
Your Name <you@example.com>
2023-09-18 18:09:19 -0700

    s

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _puts2 - that prints every other character of a string
+ * , starting with the first character, followed by a new line
+ * @str: pointer string
+ * Return: pointer
+*/
+
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
