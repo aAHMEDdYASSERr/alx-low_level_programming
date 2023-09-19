c4e3a4c043718d51f0c992d99a92af43523c7b26
Your Name <you@example.com>
2023-09-18 18:14:46 -0700

    s

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts_half - that prints half of a string, followed by a new line
+ * @str : pointer of sring
+ * Return: string
+*/
+
+
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; str[i] != '\0'; ++i)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
