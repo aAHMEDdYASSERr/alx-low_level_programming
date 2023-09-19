e347eb8d0e68cd1c5fbdf5a2a696ff52efb54009
Your Name <you@example.com>
2023-09-18 18:00:32 -0700

    s

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rev_string -  a function that reverses a string
+ * @s: pointer for string
+ * Return: string of pointer
+ *
+*/
+
+void rev_string(char *s)
+{
+	int a, b;
+	char c;
+
+	for (a = 0; s[a] != '\0'; ++a)
+		;
+
+	for (b = 0; b < a / 2; i++)
+	{
+		c = s[b];
+		s[b] = s[a - 1 - b];
+		s[a - 1 - b] = c;
+	}
+
+
+}
