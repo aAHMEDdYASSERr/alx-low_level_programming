d14f8fc92692da368ba6ca2256a8662e323d77a5
Your Name <you@example.com>
2023-09-18 18:37:55 -0700

    s

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,10 +2,23 @@
 /**
  * print_array - that prints n elements of an array of integers
  *				, followed by a new line
+ * @a: printer one
+ * @n: printer two
+ * Return: array
  *
 */
 
 
 void print_array(int *a, int n)
 {
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != (n - 1))
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+	}
+	printf("\n");
 }
