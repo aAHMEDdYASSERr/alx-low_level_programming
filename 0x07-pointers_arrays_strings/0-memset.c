4115bd2456eed3b5044bb3e5327ba1d1ab0dbaab
Your Name <you@example.com>
2023-09-26 04:04:10 -0700

    a

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _memset - function fills the first n bytes of the memory area 
+ * _memset - function fills the first n bytes of the memory area
  * pointed to by s with the constant byte b
  *	Returns a pointer to the memory area s
  * @s: frist pointer
