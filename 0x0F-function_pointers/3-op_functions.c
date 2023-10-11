0f98dd12e1b0c02c95811d32b6a0181f07968c8d
Your Name <you@example.com>
2023-10-11 12:30:53 -0700

    a

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two int
+ * @a: int
+ * @b: int two
+ *
+ * Return: the sum
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - sub
+ * @a: int
+ * @b: two int
+ *
+ * Return: sub
+*/
+int op_sub(int a, int b)
+{
+	return (a - b)
+}
+
+/**
+ * op_mul - sub
+ * @a: int
+ * @b: two int
+ *
+ * Return: sub
+*/
+int op_mul(int a, int b)
+{
+        return (a * b);
+}
+
+/**
+ * op_sub - sub
+ * @a: int
+ * @b: two int
+ *
+ * Return: sub
+*/
+int op_div(int a, int b)
+{
+        return (a / b);
+}
+
+/**
+ * op_sub - sub
+ * @a: int
+ * @b: two int
+ *
+ * Return: sub
+*/
+int op_mod(int a, int b)
+{
+        return (a % b)
+}
