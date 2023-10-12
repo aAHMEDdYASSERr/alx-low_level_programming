b85e460493ade1341615149ae19dead6047597af
Your Name <you@example.com>
2023-10-12 15:59:30 -0700

    a

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -56,7 +56,7 @@ void format_string(char *separator, va_list ap)
 
 void print_all(const char * const format, ...)
 {
-	int i = 0, j;
+	| int i = 0, j;
 	char *separator = "";
 	va_list ap;
 	token_t tokens[] = {
@@ -67,7 +67,7 @@ void print_all(const char * const format, ...)
 		{NULL, NULL}
 	};
 	va_start(ap, format);
-       	while (fomat && format[i])
+       	while (format && format[i])
 	{
 		j = 0;
 		while (tokens[j].token)
