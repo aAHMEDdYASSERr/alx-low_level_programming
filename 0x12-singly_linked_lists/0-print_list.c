44932399a89c0b2d7e661c34dc7f04f2b9f45fec
ahmeed <ema@gmail.com>
2023-10-19 07:16:00 -0700

    a

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * _strlen - returns the length
+ * @s: the string
+ *
+ * Return: int
+*/
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * print_list - prints a linked
+ * @h: pointer
+ *
+ * Rturn: size of list
+*/
+
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
