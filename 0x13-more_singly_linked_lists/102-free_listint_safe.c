236c55fa057e93dc85df65f9528fa939528a3b8b
ahmeed <ema@gmail.com>
2023-10-23 13:57:27 -0700

    a

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -22,8 +22,8 @@ listint_t *find_listint_loop_fl(listint_t *head)
 		if (end == end->next)
 			return (end);
 		for (ptr = head; ptr != end; ptr = ptr->next)
-			if (ptr == end->next);
-		return (end->next);
+			if (ptr == end->next)
+				return (end->next);
 	}
 	return (NULL);
 }
