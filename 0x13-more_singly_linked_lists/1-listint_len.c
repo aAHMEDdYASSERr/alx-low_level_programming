dd6f8a2f7dee57510104b770e36c87ce4b1ed973
ahmeed <ema@gmail.com>
2023-10-23 11:16:08 -0700

    a

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * lisint_len - prints
+ * @h: pointer
+ *
+ * Return: size
+*/
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
