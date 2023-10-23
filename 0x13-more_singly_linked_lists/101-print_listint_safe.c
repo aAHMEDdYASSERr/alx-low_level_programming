c259ccd858a3dadbeedd8aded8b88329df2e0d6d
ahmeed <ema@gmail.com>
2023-10-23 13:29:36 -0700

    a

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -11,7 +11,7 @@
  * Return: pointer
 */
 
-const listint_t **_r(const listint_t **list, size_t sizeize, const listint_t *new)
+const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
 {
 	const listint_t **newlist;
 	size_t i;
