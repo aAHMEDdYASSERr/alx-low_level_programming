d632094b6c1aa375d51e9ba599bf72fe868ee28e
ahmeed <ema@gmail.com>
2023-10-23 11:13:24 -0700

    a

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints a linked
+ * @h: pointer
+ *
+ * Return: size
+*/
+
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
